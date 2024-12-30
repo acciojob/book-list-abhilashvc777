116c43383f7dabfba15298dad843141e6b99ecd0
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-31 01:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,20 @@ const isbn = document.querySelector("#isbn");
 const btn = document.querySelector("#submit");
 const table = document.querySelector(".table")
 const tbody = document.querySelector("#book-list")
-btn.addEventListener("click", (e)=>{
-	// let a = title.value
-	// let b = author.value
-	// let c = isbn.value
+
+btn.addEventListener("click", ()=>{
 	const td = `<td>${title.value}</td>
 			<td>${author.value}</td>
 			<td>${isbn.value}</td>
 			<td><button class="cancelbtn">x</button><td>`
-	const tr = document.createElement("tr")
+   const	tr = document.createElement("tr")
 	tr.innerHTML = td
 	tbody.appendChild(tr)
-	const cancelbtn = document.querySelectorAll(".cancelbtn")
-	// console.log(cancelbtn)
-	
-	// cancelbtn[i].addEventListener("click", (e)=>{
-	// 	console.log(e)
-	// })
+const cancelbtn = document.querySelectorAll(".cancelbtn")
+	let i = 0;
+  tbody.addEventListener("click",(e)=>{
+	  i++
+	  // let cancel = e.target.
+	e.target.closest("tr").remove()
+  })
 })
